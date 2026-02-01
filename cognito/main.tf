99a35d74f96345fe05002dd218f89e113baa1199
ToxicalNoob3062 <rahat3062@gmail.com>
2026-01-31 23:37:26 -0500

    Update Cognito user pool schema: adjust min_length for name attribute and add verification message template

--- cognito/main.tf
+++ cognito/main.tf
@@ -12,7 +12,7 @@ resource "aws_cognito_user_pool" "user_pool" {
     mutable             = true
     required            = false
     string_attribute_constraints {
-      min_length = 1
+      min_length = 0
       max_length = 50 # name usually not very long
     }
   }
@@ -35,6 +35,17 @@ resource "aws_cognito_user_pool" "user_pool" {
     require_lowercase = true
     require_numbers   = true
     require_symbols   = true
+  }
+
+  # how users verify their email.
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_message        = "Your Pledge code: {####}"
+    email_subject        = "Pledge Proof - Verify Email"
+  }
+
+  lifecycle {
+    ignore_changes = [ schema ]
   }
 
   # tags
