826644b5a2b7e66c2a9f1d1f37284b313e02cfae
ToxicalNoob3062 <rahat3062@gmail.com>
2026-02-03 21:16:11 -0500

    Fix output references for unified event bus in outputs.tf

--- event-bus/outputs.tf
+++ event-bus/outputs.tf
@@ -1,9 +1,9 @@
 output "unified_event_bus_id" {
-  value = data.aws_cloudwatch_event_bus.unified_bus.id
+  value = aws_cloudwatch_event_bus.unified_bus.id
 }
 
 output "unified_event_bus_arn" {
-  value = data.aws_cloudwatch_event_bus.unified_bus.arn
+  value = aws_cloudwatch_event_bus.unified_bus.arn
 }
 
 output "event_bridge_dlq_id" {
